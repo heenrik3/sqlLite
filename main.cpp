2bdb6c8896a22d57977c47083967a2b7dbba8e36
henrik3 <henriqlh@gmail.com>
2021-01-26 15:06:21 -0300

    testing

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 int main(int argc, char const *argv[])
 {
     Database *db = new Database();
-
     db->createOrOpenDatabase();
+
+    db->changeLocale("pt");
+    db->createOrOpenDatabase();
+
+
     delete db;
     return 0;
 }
