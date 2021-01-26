63ce10cf35722261a335cabd2b4683447917ce89
henrik3 <henriqlh@gmail.com>
2021-01-26 10:08:43 -0300

    added utilityFunctions

---
+++ include/utilityFunctions.hpp
@@ -0,0 +1,25 @@
+//
+//  utilityFunctions.hpp
+//  sqlLite
+//
+//  Created by henrik3 on 26/01/2021.
+//
+//
+
+#ifndef utilityFunctions_hpp
+#define utilityFunctions_hpp
+
+#include <iostream>
+
+#ifdef _WIN32
+#define clears "cls" // windows
+#endif
+
+#ifdef linux
+#define clears "clear" // linux
+#endif
+
+void clear();
+
+
+#endif /* utilityFunctions_hpp */
