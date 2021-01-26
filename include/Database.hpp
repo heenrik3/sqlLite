da2e5285bbae22dd3a101814be283bdccbb8acaf
henrik3 <henriqlh@gmail.com>
2021-01-26 10:11:31 -0300

    removed clear(), now on utilityFuncs removed getMapValues() unused some code moved to newly created createDatabase() func

--- include/Database.hpp
+++ include/Database.hpp
@@ -9,10 +9,9 @@
 
 #include "sqlite3.h"
 #include "ResourceBundle.hpp"
+#include "utilityFunctions.hpp"
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName);
-
-void clear();
 
 class Database
 {
@@ -31,12 +30,14 @@ class Database
       ResourceBundle *resources;                  // holds a map bundle with strings resources to be used
 
       bool isDatabaseOpen();                      // checks if there's a database connection available
-      std::string getSqlCommand();                // returns a string containing an sql command by the user
+      std::string getSqlCommand();                // asks for an input and returns a string containing an sql command by the user
       void execSQL(std::string sql,
                     std::map<std::string,
                      std::string> m);             // receives a string with an sql command and a map
       void setResourceAndExec(std::map<std::string,
-                                std::string> m);  // receives a map with resources
+                                std::string> m);  // receives a map with string resources
+
+      bool createDatabase();
 
 };
 #endif // DATABASE_HPP
